58bc69d6b49010d0b2176cbd62de189875fdb078
G_Dychala <g.dychala@gmail.com>
2023-01-19 12:35:09 +0100

    added proper sorty function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const arr = document.getElementById('arr');
 document.getElementById("ok").addEventListener('click', getData);
 let nodesIdTab = [];
 let nodesTab = [];
+const stPoint = {lat:52.1421189, lon:22.2503961};
 
 function getData() {
     arr.innerHTML = `<br/><b>zestaw węzłów lini: <br/></b>`;
@@ -27,16 +28,25 @@ function getData() {
                             let node = xmldoc.getElementsByTagName('node')[0];
                             let lat = node.getAttribute('lat');
                             let lon = node.getAttribute('lon');
-                           nodesTab.push(`[${lat}, ${lon}]`);
+                            nodesTab.push(`${lat}, ${lon}`);
                             arr.innerHTML += `[${lat}, ${lon}], `;
                         });
                 }
 
             });
     }
-  setTimeout( ()=> {nodesTab.sort();
-    let filNodesTab = nodesTab.filter((item, index) => nodesTab.indexOf(item)===index);
-arr.innerHTML += `<br/><br/>posortowana tablica: <br/>${filNodesTab}`},2000);
+    setTimeout(() => {
+        let filNodesTab = nodesTab.filter((item, index) => nodesTab.indexOf(item) === index);
+        let twoDimTab = filNodesTab.map(function(row){return row.split(",")});
+        twoDimTab = twoDimTab.map(row => row.map(elem => parseFloat(elem)));
+        let sortfun = (a, b) =>{let distanceA = Math.sqrt(Math.abs((a[0]-stPoint.lat) * (a[0]-stPoint.lat) + (a[1]-stPoint.lon) * (a[1]-stPoint.lon)));
+                                let distanceB = Math.sqrt(Math.abs((b[0]-stPoint.lat) * (b[0]-stPoint.lat) + (b[1]-stPoint.lon) * (b[1]-stPoint.lon)));
+                            return  distanceA - distanceB};
+      let sortTab = twoDimTab.sort(sortfun);
+      arr.innerHTML += `<br/><br/>posortowana tablica węzłów:<br/>`
+      for(let m=0; m<sortTab.length; m++){
+        arr.innerHTML += `[${sortTab[m]}], `}
+    }, 5000);
 };
 
 
