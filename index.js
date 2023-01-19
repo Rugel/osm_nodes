fc4b4bfd9f077ee345f224d89648053e8aeb12db
G_Dychala <g.dychala@gmail.com>
2023-01-19 23:14:06 +0100

    optymalized and reduce

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const arr = document.getElementById('arr');
 document.getElementById("ok").addEventListener('click', getData);
 let nodesIdTab = [];
 let nodesTab = [];
-let stPoint = {lat:0.0, lon:0.0};
+let stPoint = [];
 
 function getData() {
     arr.innerHTML = `<br/><b>zestaw węzłów lini w losowej kolejności: <br/><br/></b>`;
     const idsData = document.getElementById("input").value;
     const pointV = document.getElementById('point').value;
     const pTab = pointV.split(" ");
-    stPoint = {lat:pTab[0], lon:pTab[1]};
+    // stPoint = {lat:pTab[0], lon:pTab[1]};
     const tabData = idsData.split(' ');
     let nodeId;
     for (let i = 0; i < tabData.length; i++) {
@@ -38,17 +38,47 @@ function getData() {
 
             });
     }
+
+    fetch(`https://www.openstreetmap.org/api/0.6/way/${tabData[0]}`)
+        .then(res => res.text())
+        .then(data => new window.DOMParser().parseFromString(data, "text/xml"))
+        .then(xmlDoc => {
+            let pointCol = xmlDoc.getElementsByTagName('nd');
+            let pointId = pointCol[0].getAttribute('ref');
+            fetch(`https://www.openstreetmap.org/api/0.6/node/${pointId}`)
+                .then(response => response.text())
+                .then(dane => new window.DOMParser().parseFromString(dane, "text/xml"))
+                .then(xmldoc => {
+                    let pkt = xmldoc.getElementsByTagName('node')[0];
+                    let lt = pkt.getAttribute('lat');
+                    let ln = pkt.getAttribute('lon');
+                    stPoint.push(lt);
+                    stPoint.push(ln);
+                  
+                })
+        });
+
     setTimeout(() => {
         let filNodesTab = nodesTab.filter((item, index) => nodesTab.indexOf(item) === index);
-        let twoDimTab = filNodesTab.map(function(row){return row.split(",")});
+        let twoDimTab = filNodesTab.map(function (row) { return row.split(",") });
         twoDimTab = twoDimTab.map(row => row.map(elem => parseFloat(elem)));
-        let sortfun = (a, b) =>{let distanceA = Math.sqrt(Math.abs((a[0]-stPoint.lat) * (a[0]-stPoint.lat) + (a[1]-stPoint.lon) * (a[1]-stPoint.lon)));
-                                let distanceB = Math.sqrt(Math.abs((b[0]-stPoint.lat) * (b[0]-stPoint.lat) + (b[1]-stPoint.lon) * (b[1]-stPoint.lon)));
-                            return  distanceA - distanceB};
-      let sortTab = twoDimTab.sort(sortfun);
-      arr.innerHTML += `<br/><br/>posortowany zestaw węzłów względem punktu "${stPoint.lat}, ${stPoint.lon}":<br/><br/>`;
-      for(let m=0; m<sortTab.length; m++){
-        arr.innerHTML += `[${sortTab[m]}], `}
+        stPoint = [parseFloat(stPoint[0]), parseFloat(stPoint[1])];
+                    console.log(stPoint);
+        // let sortfun = (a, b) =>{let distanceA = Math.sqrt(Math.abs((a[0]-stPoint.lat) * (a[0]-stPoint.lat) + (a[1]-stPoint.lon) * (a[1]-stPoint.lon)));
+        //  let distanceB = Math.sqrt(Math.abs((b[0]-stPoint.lat) * (b[0]-stPoint.lat) + (b[1]-stPoint.lon) * (b[1]-stPoint.lon)));
+        //  return  distanceA - distanceB};
+
+        let sortfun = (a, b) => {
+            let distanceA = Math.sqrt(Math.abs((a[0] - stPoint[0]) * (a[0] - stPoint[0]) + (a[1] - stPoint[1]) * (a[1] - stPoint[1])));
+            let distanceB = Math.sqrt(Math.abs((b[0] - stPoint[0]) * (b[0] - stPoint[0]) + (b[1] - stPoint[1]) * (b[1] - stPoint[1])));
+            return distanceA - distanceB
+        };
+
+        let sortTab = twoDimTab.sort(sortfun);
+        arr.innerHTML += `<br/><br/>posortowany zestaw węzłów względem punktu "${stPoint.lat}, ${stPoint.lon}":<br/><br/>`;
+        for (let m = 0; m < sortTab.length; m++) {
+            arr.innerHTML += `[${sortTab[m]}], `
+        }
     }, 5000);
 };
 
